00baa05511edaee03fc13798b380378a61360789
AbdalrhmanMoh <abd0004@hotmail.com>
2023-12-12 18:02:25 +0200

    almost done

---
+++ script.js
@@ -0,0 +1,14 @@
+let vars = [-1, 1, 1, 1,1];
+function openorclose(name) {
+  let ButtonNumber = name.id[1];
+  if (vars[ButtonNumber]) {
+    document.getElementById(name.id).style.backgroundImage = "url('assets/images/icon-minus.svg')";
+    document.getElementById("p" + ButtonNumber).style.visibility = 'visible';
+    document.getElementById("p" + ButtonNumber).style.height ="auto";
+  } else {
+    document.getElementById(name.id).style.backgroundImage = "url('assets/images/icon-plus.svg')";
+    document.getElementById("p" + ButtonNumber).style.visibility = 'hidden';
+    document.getElementById("p" + ButtonNumber).style.height = "0px";
+  }
+  vars[ButtonNumber] ^= 1;
+}
